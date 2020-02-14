3fface5e9d8869eded42015357f90fcf980951cf
VarunSai23 <imvarun23@gmail.com>
2020-02-15 02:00:25 +0530

    Search Fixed

--- movies/views.py
+++ movies/views.py
@@ -14,10 +14,15 @@ TMDB_END_POINT = "https://api.themoviedb.org/3/"
 def home(request):
     if request.method == 'POST':
         movie_name = request.POST.get('movie_name', None)
-        return redirect('search', movie_name)
+        if movie_name != '':
+            movie_name = ''.join(movie_name.replace(" ", "_"))
+            return redirect('search', movie_name)
+        else:
+            return redirect('home')
     else:
         endpoint = TMDB_END_POINT + "movie/top_rated?"
-        params = {'api_key': settings.TMDB_API_KEY, "language": 'en-US', 'page': 1}
+        params = {'api_key': settings.TMDB_API_KEY,
+                  "language": 'en-US', 'page': 1}
         response = requests.get(endpoint, params=params)
         result = response.json()
         print("url : " + response.url)
@@ -30,7 +35,8 @@ def upcoming(request):
         return redirect('search', movie_name)
     else:
         endpoint = TMDB_END_POINT + "movie/upcoming?"
-        params = {'api_key': settings.TMDB_API_KEY, "language": 'en-US', 'page': 1}
+        params = {'api_key': settings.TMDB_API_KEY,
+                  "language": 'en-US', 'page': 1}
         response = requests.get(endpoint, params=params)
         result = response.json()
         print("url : " + response.url)
@@ -43,7 +49,8 @@ def popular(request):
         return redirect('search', movie_name)
     else:
         endpoint = TMDB_END_POINT + "movie/popular?"
-        params = {'api_key': settings.TMDB_API_KEY, "language": 'en-US', 'page': 1}
+        params = {'api_key': settings.TMDB_API_KEY,
+                  "language": 'en-US', 'page': 1}
         response = requests.get(endpoint, params=params)
         result = response.json()
         print("url : " + response.url)
@@ -52,11 +59,16 @@ def popular(request):
 
 def search(request, query):
     if query != "":
+        query = ''.join(query.replace("_", "%"))
+        heading_query = ''.join(query.replace("%", " "))
         endpoint = TMDB_END_POINT + "search/movie?"
-        params = {'api_key': settings.TMDB_API_KEY, "language": 'en-US', 'page': 1, 'query': query}
+        params = {'api_key': settings.TMDB_API_KEY,
+                  "language": 'en-US', 'page': 1, 'query': query}
         response = requests.get(endpoint, params=params)
+        print(response.url)
         result = response.json()
-        return render(request, 'movies/home.html', {'results': result, 'prev_query': query})
+        print("Heading query = " + heading_query)
+        return render(request, 'movies/home.html', {'results': result, 'prev_query': heading_query})
 
 
 def detail(request, movie_id):
@@ -75,7 +87,8 @@ def detail(request, movie_id):
     result_video = response_video.json()
     rel_date = result["release_date"]
     listDates = rel_date.split("-")
-    x = datetime.datetime(int(listDates[0]), int(listDates[1]), int(listDates[2]))
+    x = datetime.datetime(int(listDates[0]), int(
+        listDates[1]), int(listDates[2]))
     formatted_rel_date = x.strftime("%b %d %Y")
     comments = Comment.objects.filter(movie_id=movie_id)
     return render(request, 'movies/detail.html', {'results': result, 'formatted_rel_date': formatted_rel_date,
@@ -91,7 +104,8 @@ def user_home_page(request, username):
     json_result = {}
     params = {'api_key': settings.TMDB_API_KEY, "language": 'en-US'}
     for i in range(len(collections)):
-        endpoint = TMDB_END_POINT + "movie/" + str(collections[i].movie_id) + '?'
+        endpoint = TMDB_END_POINT + "movie/" + \
+            str(collections[i].movie_id) + '?'
         response = requests.get(endpoint, params=params)
         result = response.json()
         results.append(result)
@@ -121,5 +135,6 @@ def remove_from_collection(request, movie_id):
 def add_comment(request, movie_id):
     if request.method == "POST":
         user = request.user
-        Comment.objects.create(comment_text=request.POST.get("comment_text"), uploaded_by=user, movie_id=movie_id)
+        Comment.objects.create(comment_text=request.POST.get(
+            "comment_text"), uploaded_by=user, movie_id=movie_id)
     return redirect('movie_detail', movie_id)
