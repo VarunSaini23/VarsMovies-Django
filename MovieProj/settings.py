249f064f505f6f224566092a8d2fb360f461379e
VarunSai23 <imvarun23@gmail.com>
2020-02-26 20:41:51 +0530

    Responsive5

--- MovieProj/settings.py
+++ MovieProj/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TMDB_API_KEY = config('TMDB_API_KEY', default='')
 
